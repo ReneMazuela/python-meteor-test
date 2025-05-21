58b07f269d86dee87184eaaf1ba14a1f24a4469c
renemazuela <rjmazuela@gmail.com>
2025-05-21 00:44:58 -0700

    Fix Port Config For Galaxy

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html')  # or just return "Hello, World!"
 
 def create_app():
     return app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Bind to PORT environment variable if defined (e.g. GalaxyCloud, Heroku)
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
